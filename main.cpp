3be08732eb35092a922a534aa91ad1c1a7d4e957
Fake ID <junayeed@protonmail.com>
2017-08-22 16:32:46 +0600

    Added transition

--- main.cpp
+++ main.cpp
@@ -17,13 +17,17 @@ int main() {
     while (true) {
 //        Update particles
 //        Draw particles
+        int elapsed = SDL_GetTicks();
+        auto green = (unsigned char) ((1 + sin(elapsed * 0.0001)) * 128);
+        auto red = (unsigned char) ((1 + sin(elapsed * 0.0002)) * 128);
+        auto blue = (unsigned char) ((1 + sin(elapsed * 0.0003)) * 128);
+
         for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
             for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
-                screen.setPixel(x, y, 128, 0, 255);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
 
-        screen.setPixel(400, 300, 255, 255, 255);
 
 //        Draw the screen
         screen.update();
